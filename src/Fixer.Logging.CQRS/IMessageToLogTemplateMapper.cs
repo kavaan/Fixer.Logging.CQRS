f0acaf3ee2f383fd5cb9802510e0a6c75196cd59
Samin <Samin@DESKTOP-B8FQBKL>
2020-08-17 22:48:27 +0430

    first commit

---
+++ src/Fixer.Logging.CQRS/IMessageToLogTemplateMapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fixer.Logging.CQRS
+{
+    public interface IMessageToLogTemplateMapper
+    {
+        HandlerLogTemplate Map<TMessage>(TMessage message) where TMessage : class;
+    }
+}
